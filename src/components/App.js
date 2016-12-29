a56b345f05e4f86d0459c41d8420ed5fff587572
wonny <jumpegg@naver.com>
2016-12-29 23:27:57 +0900

    first

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+import Content from './Content';
+
+class App extends React.Component{
+  render(){
+      return(
+        <div>
+          <Header title={ this.props.headerTitle }/>
+          <Content title={ this.props.contentTitle }
+                    body={ this.props.contentBody } />
+        </div>
+    );
+  }
+}
+
+App.defaultProps = {
+  headerTitle: 'Default header',
+  contentTitle: 'Default contentTitle',
+  contentBody: 'Default contentBody'
+};
+
+export default App;
